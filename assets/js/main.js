6e766f9b2fe80051a04cf97c2b4b3d9390748f5f
Jacksonson24 <Jackforfar86@gmail.com>
2026-07-19 17:14:48 +0100

    Closetofinito

--- assets/js/main.js
+++ assets/js/main.js
@@ -317,22 +317,23 @@
 
 			$window.on('keyup', function(event) {
 
-				switch (event.keyCode) {
+	// Do not close the project article while the lightbox is open.
+	if ($imageLightbox && $imageLightbox.hasClass('open'))
+		return;
 
-					case 27:
+	switch (event.keyCode) {
 
-						// Article visible? Hide.
-							if ($body.hasClass('is-article-visible'))
-								$main._hide(true);
+		case 27:
 
-						break;
+			if ($body.hasClass('is-article-visible'))
+				$main._hide(true);
 
-					default:
-						break;
+			break;
 
-				}
-
-			});
+		default:
+			break;
+	}
+});
 
 			$window.on('hashchange', function(event) {
 
@@ -397,4 +398,214 @@
 					$window.on('load', function() {
 						$main._show(location.hash.substr(1), true);
 					});
+							// =========================================
+	// Fullscreen image lightbox gallery
+	// =========================================
+
+	var $imageLightbox = $('#image-lightbox'),
+		$imageLightboxImage = $imageLightbox.find('.image-lightbox-content'),
+		$imageLightboxClose = $imageLightbox.find('.image-lightbox-close'),
+		$imageLightboxPrevious = $imageLightbox.find('.image-lightbox-previous'),
+		$imageLightboxNext = $imageLightbox.find('.image-lightbox-next'),
+		$imageLightboxCounter = $imageLightbox.find('.image-lightbox-counter'),
+		$lightboxLinks = $(),
+        currentLightboxIndex = 0;
+
+	function updateLightboxImage() {
+
+		var $currentLink = $lightboxLinks.eq(currentLightboxIndex),
+			$currentImage = $currentLink.find('img');
+
+		$imageLightboxImage.attr({
+			src: $currentLink.attr('href'),
+			alt: $currentImage.attr('alt') || 'Fullscreen project screenshot'
+		});
+
+		$imageLightboxCounter.text(
+			(currentLightboxIndex + 1) + ' / ' + $lightboxLinks.length
+		);
+
+	}
+
+	function openImageLightbox(index) {
+
+		currentLightboxIndex = index;
+
+		updateLightboxImage();
+
+		$imageLightbox
+			.addClass('open')
+			.attr('aria-hidden', 'false');
+
+		$body.addClass('lightbox-open');
+
+	}
+
+	function closeImageLightbox() {
+
+		$imageLightbox
+			.removeClass('open')
+			.attr('aria-hidden', 'true');
+
+		$body.removeClass('lightbox-open');
+
+		$imageLightboxImage.attr({
+			src: '',
+			alt: ''
+		});
+
+	}
+
+	function showPreviousLightboxImage() {
+
+		currentLightboxIndex--;
+
+		if (currentLightboxIndex < 0)
+			currentLightboxIndex = $lightboxLinks.length - 1;
+
+		updateLightboxImage();
+
+	}
+
+	function showNextLightboxImage() {
+
+		currentLightboxIndex++;
+
+		if (currentLightboxIndex >= $lightboxLinks.length)
+			currentLightboxIndex = 0;
+
+		updateLightboxImage();
+
+	}
+
+		// Open a selected image and build its project-specific gallery.
+	document.addEventListener('click', function(event) {
+
+		var lightboxLink = event.target.closest('a.lightbox-image');
+
+		if (!lightboxLink)
+			return;
+
+		event.preventDefault();
+		event.stopPropagation();
+		event.stopImmediatePropagation();
+
+		var groupName = lightboxLink.getAttribute('data-lightbox-group');
+
+		if (groupName) {
+
+			$lightboxLinks = $(
+				'a.lightbox-image[data-lightbox-group="' + groupName + '"]'
+			);
+
+		}
+		else {
+
+			// An ungrouped image opens by itself.
+			$lightboxLinks = $(lightboxLink);
+
+		}
+
+		var selectedIndex = $lightboxLinks.index(lightboxLink);
+
+		if (selectedIndex !== -1)
+			openImageLightbox(selectedIndex);
+
+	}, true);
+
+	// Close button.
+	$imageLightboxClose.on('click', function(event) {
+
+		event.preventDefault();
+		event.stopPropagation();
+		event.stopImmediatePropagation();
+
+		closeImageLightbox();
+
+		return false;
+
+	});
+
+	// Previous button.
+	$imageLightboxPrevious.on('click', function(event) {
+
+		event.preventDefault();
+		event.stopPropagation();
+		event.stopImmediatePropagation();
+
+		showPreviousLightboxImage();
+
+		return false;
+
+	});
+
+	// Next button.
+	$imageLightboxNext.on('click', function(event) {
+
+		event.preventDefault();
+		event.stopPropagation();
+		event.stopImmediatePropagation();
+
+		showNextLightboxImage();
+
+		return false;
+
+	});
+
+	// Clicking the image itself does nothing.
+	$imageLightboxImage.on('click', function(event) {
+
+		event.preventDefault();
+		event.stopPropagation();
+		event.stopImmediatePropagation();
+
+	});
+
+	// Close when clicking only the dark background.
+	$imageLightbox.on('click', function(event) {
+
+		event.preventDefault();
+		event.stopPropagation();
+		event.stopImmediatePropagation();
+
+		if (event.target === this)
+			closeImageLightbox();
+
+	});
+
+	// Keyboard controls.
+	document.addEventListener('keyup', function(event) {
+
+		if (!$imageLightbox.hasClass('open'))
+			return;
+
+		switch (event.key) {
+
+			case 'Escape':
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+
+				closeImageLightbox();
+				break;
+
+			case 'ArrowLeft':
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+
+				showPreviousLightboxImage();
+				break;
+
+			case 'ArrowRight':
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+
+				showNextLightboxImage();
+				break;
+
+		}
+
+	}, true);
 })(jQuery);
